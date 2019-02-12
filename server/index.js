711bfd05264474b84e6c2aff9fb8b32dba935af8
youssefwalid7 <47348750+youssefwalid7@users.noreply.github.com>
2019-02-12 23:00:28 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const students = [
     { id: "28-09121", name: "Omar Sherif", github_username: "osheriff", email: "omarr@whatever.com" },
     { id: "21-094123", name: "Mathew White", github_username: "matheww", email: "matheww@whatever.com" },
     { id: "15-10312", name: "Dom Sundle", github_username: "domss", email: "domss.whatever.com" },
-    { id: "7223", name: "Gehad Ismail", github_username: "Gehad93", email: "gehad.ismail@guc.edu.eg" }
-    { id:"40-3622", name:'Youssef Walid', github_username: "youssefwalid7", email: "youssef.omar@student.guc.edu.eg"}
+    { id: "7223", name: "Gehad Ismail", github_username: "Gehad93", email: "gehad.ismail@guc.edu.eg" },
+    { id:"40-3622", name:'Youssef Walid Abselrahman', github_username: "youssefwalid7", email: "youssef.omar@student.guc.edu.eg"},
 
 ];
 
